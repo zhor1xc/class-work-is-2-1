686df55f4613cf9f9e5be59760e9336cc6b9beaf
zhor1xc <name@example.ru>
2024-02-05 13:49:13 +0300

    Выполнили работу с объектами

--- tusk_3/main.js
+++ tusk_3/main.js
@@ -1,23 +1,16 @@
-//const students = ["Андрей", "Иван", "Стас"];
-//students[50] = "Индекс 50";
-//console.log(students);
+let college = [
+    { name: "Tom", age: 23, group: "ИС-2-2" },
+    { name: "Bob", age: 15, group: "ИС-2-1" },
+    { name: "Stan", age: 23, group: "ИС-2-2" },
+    { name: "Harry", age: 17, group: "ИС-4-1" },
+];
 
+let count = 2
+let studentAge = college[count]["age"]
+let studentName = college[count]["name"]
 
-//let college = [ ["Андрей", "Иван", "Стас"], [205, 111, 304] ];
-//college[1][1] = 112
-//let kab = college[1][1]
-//console.log(kab);
-
-let number = [ 1, 2, 3, 4, 5, 6, 7 ]
-
-const countLenght = 5;
-
-if ( number.length === countLenght ) {
-    console.log("ok");
-} else if ( number.length < countLenght ) {
-    let result = countLenght - number.length;
-    console.log(`Длина вашего массива: ${number.length}. Длину массива нужно увеличить до ${countLenght}. Прибавьте к массиву значение ${result}`);
-} else if ( number.length > countLenght ) { 
-    result = number.length - countLenght;
-    console.log(`Длина вашего массива: ${number.length}. Длину массива нужно уменьшить до ${countLenght}. Удалите из массива значение ${result}`)
+if ( studentAge >= 18 ) {
+    console.log(`Студент ${studentName} совершеннолетний. Ему ${studentAge} лет`);
+} else if ( studentAge < 18) {
+    console.log(`Студент ${studentName} не совершеннолетний. Ему ${studentAge} лет`); 
 }
